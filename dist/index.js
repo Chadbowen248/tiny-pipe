0a5152e1c322e8bf6bafd854c824e03a3fc8a8fe
Impactionplatform <Chadbowen248@gmail.com>
2018-08-19 20:12:44 -0400

    fixing output mistake

--- dist/index.js
+++ dist/index.js
@@ -1,22 +1,3 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-    value: true
-});
-var tiny = {
-    pipe: function pipe() {
-        for (var _len = arguments.length, fns = Array(_len), _key = 0; _key < _len; _key++) {
-            fns[_key] = arguments[_key];
-        }
-
-        return function (start) {
-            return fns.reduce(function (x, y) {
-                return y(x);
-            }, start);
-        };
-    }
-};
-
-exports.default = tiny;
-
-module.exports = tiny;
+export const tiny = {
+    pipe: (...fns) => start => fns.reduce((x,y) => y(x), start)
+}
